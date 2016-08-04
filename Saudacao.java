ca83d62849f20225299d321b1661450128566375
Flavio Luiz Garcia <flavio.garcia@b2wdigital.com>
2016-08-04 08:42:17 -0300

    Passagem do nome como parametro

--- Saudacao.java
+++ Saudacao.java
@@ -1,5 +1,6 @@
 class Saudacao {
 	public static void main(String args[]){
-		System.out.println("Benvindo ao curso de Java");
+		System.out.println(args[0]
+			+ " Benvindo ao curso de Java");
 	}
 }
